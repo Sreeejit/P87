3a1a8567319d76e32819ac68b7463ebd22d0fb32
Sreeejit <77166984+Sreeejit@users.noreply.github.com>
2021-05-28 23:46:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+canvas = new fabric.Canvas("myCanvas");
+window.addEventListener('keydown', mykeydown)
+block_img_width = 30;
+block_img_height = 30;
+player_x = 10;
+player_y = 10;
+player_object = "";
+block_object = "";
+function player_update(){
+fabric.Image.fromURL("player.png", function(Img){
+player_object = Img;
+
+player_object.scaleToWidth(150);
+player_object.scaleToHeight(140);
+player_object.set({
+top:player_y,
+left:player_x
+});
+canvas.add(player_object)
+});
+}
+
+
+function new_image(get_img){
+    fabric.Image.fromURL(get_img, function(Img){
+     block_object = Img;
+    
+    block_object.scaleToWidth(SpanW);
+    block_object.scaleToHeight(SpanH);
+    block_object.set({
+    top:player_y,
+    left:player_x
+    });
+    canvas.add(block_object)
+    });
+    }
+
+
+
+    function mykeydown(e){
+keyPressed = e.keycode;
+console.log(keyPressed);
+
+
+if (e.shiftKey == true&&keyPressed == '72'){
+    block_img_height = block_img_height + 5;
+    block_img_width = block_img_width + 5;
+    document.getElementById("SpanW").innerHTML = block_img_width;
+    document.getElementById('SpanH').innerHTML = block_img_height;
+}
+
+if (e.shiftKey == true&&keyPressed == '69'){
+    block_img_height = block_img_height - 5;
+    block_img_width = block_img_width - 5;
+    document.getElementById("SpanW").innerHTML = block_img_width;
+    document.getElementById('SpanH').innerHTML = block_img_height;
+}
+
+
+if(keyPressed == '70'){
+ new_image("ironman_face.png");   
+ console.log("f")
+}
+
+if(keyPressed == '66'){
+new_image("spiderman_body.png");
+console.log("b")
+
+}
+
+
+if(keyPressed == '76'){
+new_image("hulk_legs.png")
+console.log("l")
+
+}
+
+
+if(keyPressed == "82"){
+new_image("thor_right_hand.png");
+console.log("r")
+}
+
+if(keyPressed == "72")
+{
+ new_image("captain_america_left_hand.png");
+ console.log("h")}
+
+
+ if(keyPressed == "38"){
+up();
+console.log("up")}
+
+if(keyPressed == "40"){
+down();
+console.log("down")}
+
+if(keyPressed == "37"){
+left();
+console.log("left")
+}
+
+
+if(keyPressed == "39"){
+right();
+console.log("right")
+}
+
+
+
+    }
